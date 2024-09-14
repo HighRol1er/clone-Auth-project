e80621a26fa3661edd954ff9bc20f3521c9513d7
HunterJoe <hunterjoe9999@gmail.com>
2024-09-14 15:59:22 +0900

    set backend

--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -1,4 +1,4 @@
-import { VERIFICATION_EMAIL_TEMPLATE } from "./emailTemplates.js"
+import { PASSWORD_RESET_REQUEST_TEMPLATE, PASSWORD_RESET_SUCCESS_TEMPLATE, VERIFICATION_EMAIL_TEMPLATE } from "./emailTemplates.js"
 import { mailtrapClient, sender } from "./mailtrap.js"
 
 export const sendVerificationEmail = async (email, verificationToken) => {
@@ -38,5 +38,41 @@ export const sendWelcomeEmail = async (email, name) => {
   } catch (error) {
     console.error(`Error sending Welcome email`, error);
     throw new Error(`Error sending Welcome email: ${error}`);
+  }
+}
+
+export const sendPasswordResetEmail = async (email, resetURL) => {
+  const recipients = [{ email }];
+
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: recipients,
+      subject: "Reset your password",
+      html: PASSWORD_RESET_REQUEST_TEMPLATE.replace("{resetURL}", resetURL),
+      category: "Password Reset",
+    });
+  } catch (error) {
+    console.error(`Error sending password reset email`, error);
+    throw new Error(`Error sending password reset email : ${error}`);
+  }
+}
+
+export const sendResetSuccessEmail = async (email) => {
+  const recipients = [{ email }];
+
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: recipients,
+      subject: "Password Reset Successfully",
+      html: PASSWORD_RESET_SUCCESS_TEMPLATE,
+      category: "Password Reset",
+    });
+
+    console.log("Password reset email sent successfully", response);
+  } catch (error) {
+    console.error(`Error sending password reset success email`, error);
+    throw new Error(`Error sending password reset success email : ${error}`);
   }
 }
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import "dotenv/config";
+import cookieParser from 'cookie-parser';
 import { connectDB } from './db/connectDB.js';
 import authRoutes from "./routes/auth.route.js";
-
 
 const app = express();
 const PORT = process.env.PORT || 5010;
 
 app.use(express.json()); // allows us to parse incoming requests : req.body
+app.use(cookieParser()); // allows us to parse incoming cookies
 
 app.use("/api/auth", authRoutes);
 
